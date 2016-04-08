45f2caaa9e43ceaff52a94306cd94284197bd189
Marius Hartmann <marius@marius-x220.dk>
2016-04-08 16:37:15 +0200

    Added repr and str functions to object.author

--- blabla.py
+++ blabla.py
@@ -26,6 +26,12 @@ class Author(object):
             if book.genre == "scifi":
                 return True
         return False
-        
+
+    def __str__(self):
+        return "%s" % self.name
+    
+    def __repr__(self):
+        return "My name is %s and I have a HUGE %s" % (self.name, "nose")
+    
 if __name__ == "__main__":
     embed()
