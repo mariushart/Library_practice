f8247233d9368725f8c52c17c5691d3f98045f80
Marius Hartmann <marius@marius-x220.dk>
2016-04-08 15:30:49 +0200

    Added genre to books added class for authors

--- blabla.py
+++ blabla.py
@@ -5,15 +5,23 @@ from IPython import embed
 
 class Bog(object):
 
-    def __init__(self, title, isbn):
+    def __init__(self, title, isbn, genre):
         self.title = title
         self.isbn = isbn
-
+        self.genre = genre
+        
     def is_isbn_same_as_title(self):
         if self.title == self.isbn:
             return True
         return False
 
+class Author(object):
+    def _init_(self, name, gender):
+        self.name = name
+        self.gender = gender
 
+    def is_scifi_author(self):
+        pass
+        
 if __name__ == "__main__":
     embed()
